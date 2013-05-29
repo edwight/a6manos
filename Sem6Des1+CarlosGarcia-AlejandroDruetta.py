902565531f703a6682dd43db74d13091377830bc
Alejandro Druetta <aledruetta@gmail.com>
2013-05-29 07:57:56 -0300

    Controlando el ancho de la capa

--- Sem6Des1+CarlosGarcia-AlejandroDruetta.py
+++ Sem6Des1+CarlosGarcia-AlejandroDruetta.py
@@ -1,40 +1,49 @@
+#! /usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-''' Desarrollar un programa que permita crear diseños a partir de 
+''' Desarrollar un programa que permita crear diseños a partir de
 patrones y desplazamientos'''
 
 def main():
-	capas = input("Cuantas capas tendra el diseño?: ")
-	patron = input("Cual sera el ancho del patrón?: ")
+    capas = input("Cuántas capas tendrá el diseño?: ")
+    ancho = input("Cuál será el ancho del patrón?: ")
 
 #hacemos un for para obtener los valores del ancho del patrón
-	ancho = ""
-	for x in range(1, patron+1):
-		if x <= 9:
-			ancho = ancho + str(x)
-		elif x > 9:
-			unnumero = str(x)
-			ancho = ancho + unnumero[1]
+    guia = ""
+    for x in range(1, ancho+1):
+        if x <= 9:
+            guia = guia + str(x)
+        elif x > 9:
+            unnumero = str(x)
+            guia = guia + unnumero[1]
 
 #dibujamos los bordes del ancho del patron
-	deco = ""
-	for x in range(len(ancho)):
-		if x == 0:
-			deco += "|"
-		elif x == patron-1:
-			deco += "|"
-		else:
-			deco += " "
+    deco = ""
+    for x in range(len(guia)):
+        if x == 0:
+            deco += "|"
+        elif x == ancho-1:
+            deco += "|"
+        else:
+            deco += " "
 
 #pedimos los diseños para cada capa
-	disenio = []
-	for x in range(capas):
-		print "Introduzca el diseño de la capa",x,":"
-		print ancho
-		print deco
-		dise = raw_input()
-		disenio.append(dise)
+    disenio = []
+    capa = ""
+    for x in range(capas):
+        while True:
+            print "Introduzca el diseño de la capa",x,":"
+            print guia
+            print deco
+            dise = raw_input()
+            if len(dise) < ancho:
+                #evita que la capa sea menor que ancho completando con espacios en blanco.
+                dise = dise + " " * (ancho - len(dise))
+                break
+            elif len(dise) > ancho:
+                print "Error: ancho de la capa superior al del patrón."
+        disenio.append(dise)
 
-	print disenio
+    print disenio
 
 main()
